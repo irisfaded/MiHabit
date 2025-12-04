23f53f1cc880b826513d9b6ff21f68d195283178
Maria <maria.nikolayeva@gmail.com>
2025-12-04 13:49:21 -0500

    Created insert query for tasks and figured out slonik/zod code for such query

--- lib/actions/task-list.ts
+++ lib/actions/task-list.ts
@@ -1,12 +1,22 @@
 'use server'
 import { pool } from '@/lib/db/db_connection'
+import { z } from 'zod'
+import { sql } from 'slonik'
 
-export const createBigTask = async(prevState: any, formData: FormData) => {
-    console.log("creating big task"+formData.get('bigTask'))
-    // checks if pool is connected, just testing
-    await pool.connect(() => {
-        console.log(pool.state());
-        return pool.end()
-    })
-    return { message: "passed!", success: true }
+export const createBigTask = async (prevState: any, formData: FormData) => {
+  const content = formData.get('bigTask') as string
+  const user_id = 1 // test user id
+  console.log(content)
+  try {
+    const result = await pool.one(sql.type(
+      z.object({ id: z.number() }) // specifies the return datatype
+    )`INSERT INTO big_tasks (user_id, content) 
+    VALUES (${user_id}, '${content}') 
+    RETURNING id;`) // returns of id of inserted task
+    console.log(result)
+    return { success: true, message: result.id }
+  } catch (error) {
+    console.log(error)
+    return { success: false, message: error }
+  }
 }
--- lib/db/db_connection.ts
+++ lib/db/db_connection.ts
@@ -1,7 +1,7 @@
-import { createPool } from 'slonik'
+import { createPool, sql } from 'slonik'
 import { createPgDriverFactory } from '@slonik/pg-driver'
 import { env } from 'node:process';
 
 export const pool = await createPool(env.DATABASE_URI!, { driverFactory: createPgDriverFactory() })
+export { sql }
 
-console.log(pool.state());
